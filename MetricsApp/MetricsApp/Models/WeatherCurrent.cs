8af19930d711f04de694c505a249919523e77fed
Anand Vijayan <anand.vijayan@outlook.com>
2021-10-12 00:48:20 +0530

    MetricsApp working solution
    API to get Weather details from "WeatherApi.com"
    Uses
    1. Polly - to handle transient failures
    2. NLog - to log in file and console
    3. A custom exception handler middleware to handle exceptions in one place
    4. IHttpClientFactory - to handle all HttpClient calls at one place so that existing connections can be reused to avoid "Socket Exhaustion"

---
+++ MetricsApp/MetricsApp/Models/WeatherCurrent.cs
@@ -0,0 +1,12 @@
+﻿namespace MetricsApp.Models
+{
+    public class WeatherCurrent
+    {
+        public int last_updated_epoch { get; set; }
+        public string last_updated { get; set; }
+        public decimal temp_c { get; set; }
+        public decimal temp_f { get; set; }
+        public int is_day { get; set; }
+        public WeatherCondition condition { get; set; }
+    }
+}
---
+++ MetricsApp/MetricsApp/Startup.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Net.Http;
+using MetricsApp.Middlewares;
+using MetricsApp.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+
+namespace MetricsApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Add the ExceptionHandler middleware to handle all exceptions in one place to services collection in "Transient" scope
+            services.AddTransient<ExceptionHandler>();
+
+            services.AddControllers();
+
+            //Since ILogger not supported in this method, we need to use traditional Console.Writeline to print messages.
+            //And we need these log info only if trace is enabled in logging
+            if (Configuration["Logging:LogLevel:Default"].CompareTo("Trace") == 0)
+                Console.WriteLine("Adding HttpClient for WeatherService and setting Transient Fault handlers");
+
+            //AddHttpClient::Enabling Single connection for the service using IHttpClientFactory. It can also be used for handling multiple APIs
+            services.AddHttpClient<IWeatherService, WeatherService>(w =>
+                {
+                    w.BaseAddress = new Uri(Configuration["AppSettings:WeatherApiBaseAddress"]);
+                })
+                .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(10, _ => TimeSpan.FromSeconds(5))) //To handle 5XX and 408 http errors
+                .AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(5, TimeSpan.FromSeconds(5))) //To handle continuous Bad Requests
+                .AddPolicyHandler(request =>
+                {
+                    if (request.Method == HttpMethod.Get)
+                    {
+                        return Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(Convert.ToInt32(Configuration["AppSettings:GetMethodTimeOut"])));
+                    }
+                    return Policy.NoOpAsync<HttpResponseMessage>();
+                }); //To limit the timeout of the request based on its type.
+
+            if (Configuration["Logging:LogLevel:Default"].CompareTo("Trace") == 0)
+                Console.WriteLine("Adding HttpClient for WeatherService and setting Transient Fault handlers completed");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //Use the custom middleware created for Exception handling here
+            app.UseMiddleware<ExceptionHandler>();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
